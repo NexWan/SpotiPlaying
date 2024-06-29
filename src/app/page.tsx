c729b65795cd5a1438054da62a3a0c483b279237
nexwan <lcontrerasmartinez235@gmail.com>
2024-06-29 03:55:27 -0600

    fixed url misstype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import {useRouter} from "next/navigation";
 export default function Home() {
   const router = useRouter();
   var client_id:string = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID || "";
-  var redirect_uri = "http://localhost:3000/callback";
+  var redirect_uri = "http://spoti-playing.vercel.app/callback";
   var state = Math.random().toString(36).substring(7);
   var scope = "user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing";
 
