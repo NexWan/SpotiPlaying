45fa4f36f523cdc98c011ef8d9c43ad779beec08
nexwan <lcontrerasmartinez235@gmail.com>
2024-07-01 01:56:14 -0600

    Fixed a url misstype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import {useRouter} from "next/navigation";
 export default function Home() {
   const router = useRouter();
   var client_id:string = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID || "";
-  var redirect_uri = "http://spoti-playing.vercel.app/callback";
+  var redirect_uri = "spoti-playing-git-deploy-testing-nexwans-projects.vercel.app/callback";
   var state = Math.random().toString(36).substring(7);
   var scope = "user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing";
 
