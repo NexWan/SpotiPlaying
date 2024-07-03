2fa9bf107f9d486a34e745b5aaf87d12a32010e9
nexwan <lcontrerasmartinez235@gmail.com>
2024-07-02 21:16:03 -0600

    added cors policy for gathering user logged in api

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/api/db/userLoggedIn',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'true'
+                    }
+                ]
+            }
+        ]
+    }
 };
 
 export default nextConfig;
